11cad82118f01685b744ee60aa126ef087d3c3fb
Ry Kulp <Rdkulp@gmail.com>
2024-02-27 14:54:04 -0700

    remove tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Board from "@/components/board";
 
 export default function Home() {
   return (
-    <main className="min-h-screen items-center p-24">
+    <main>
       <Board />
     </main>
   );
