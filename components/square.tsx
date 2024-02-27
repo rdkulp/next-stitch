20e736a0b524fe5dd3db236a0f855af9a7f8dd24
Ry Kulp <Rdkulp@gmail.com>
2024-02-27 12:00:21 -0700

    Create board and x toggle

---
+++ components/square.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+  
+  function handleClick() {
+    if (value === null) {
+      setValue(
+        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="none"><path stroke="#000" stroke-linecap="round" stroke-width="4" d="M32 32 2 2M2 32 32 2"/></svg>
+      );
+    } else {
+      setValue(null);
+    }
+  }
+
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
