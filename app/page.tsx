5eeea93047bb11e4e16832c22f2efc5cfabc0ae1
Ry Kulp <Rdkulp@gmail.com>
2024-02-28 14:20:46 -0700

    Add a footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Board from "@/components/board";
 
 export default function Home() {
   return (
-    <main>
-      <Board />
-    </main>
+    <>
+      <main>
+        <Board />
+      </main>
+      <footer>
+        <p>
+          Built with 🪡 by Ry
+          <br />
+          <a href="https://rhombusrose.com">rhombusrose.com</a>
+        </p>
+      </footer>
+    </>
   );
 }
